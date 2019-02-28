8c36eb5873f09efef6121d04267f8649cf81bf7c
vtesoho <vtesoho@163.com>
2019-02-28 16:54:52 +0800

    2.28

---
+++ project/src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    test1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
